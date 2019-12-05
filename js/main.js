2d201eb53e3322d2b6687d025225fa6863c8c5ee
Jasmin Voigtlander <Jasmin@Jasmins-MacBook-Air.local>
2019-12-04 20:00:24 -0500

    added page scroll animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 (function($) {
-
 	'use strict';
 
-	// bootstrap dropdown hover
+	$('a.page-scroll').click(function() {
+    console.log('in here');
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 40
+        }, 900);
+        return false;
+      }
+    }
+  });
 
   // loader
   var loader = function() {
